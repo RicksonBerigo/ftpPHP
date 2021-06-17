e416e355247afeffde8d9db30cba483385a44590
Rickson Lucas <61435982+RicksonBerigo@users.noreply.github.com>
2021-06-17 11:26:16 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
 // Conectando com o servidor FTP
-$ftp_host = "servidorrudielle.ddns.net";
-$ftp_user = "developer";
-$ftp_pass = "Hypersoft@2020";
+$ftp_host = "server";
+$ftp_user = "user";
+$ftp_pass = "pass";
